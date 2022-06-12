530cee0059eddea123da748ae67cecedce7de16e
DmitriyBogo <superkoteman@mail.ru>
2022-06-12 11:28:45 +0300

    Changed model.py

--- model.py
+++ model.py
@@ -26,51 +26,37 @@ train_Y = LB.fit_transform(train_Y)
 val_Y = LB.fit_transform(val_Y)
 
 
-def sort_contours(cnts, method="left-to-right"):
-    reverse = False
-    i = 0
-    if method == "right-to-left" or method == "bottom-to-top":
-        reverse = True
-    if method == "top-to-bottom" or method == "bottom-to-top":
-        i = 1
+def sort_contours(cnts):
     boundingBoxes = [cv2.boundingRect(c) for c in cnts]
     (cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-                                        key=lambda b: b[1][i], reverse=reverse))
-    # return the list of sorted contours and bounding boxes
-    return (cnts, boundingBoxes)
+    key=lambda b:b[1][0]))
+    # return the list of sorted contours
+    return cnts
 
 
 def get_letters(img):
-    document_path = os.path.expanduser('~\Pictures')
-    filename = document_path + 'recognizedImage.jpg'
     letters = []
     image = cv2.imread(img)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    ret, thresh1 = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
+    ret,thresh1 = cv2.threshold(gray ,127,255,cv2.THRESH_BINARY_INV)
     dilated = cv2.dilate(thresh1, None, iterations=2)
-    cnts = cv2.findContours(dilated.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    contours, hierarchy = cv2.findContours(image=dilated.copy(), mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_SIMPLE)
-    image_copy = image.copy()
-    cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=1,
-                     lineType=cv2.LINE_AA)
-    cv2.imwrite(filename, image_copy)
-    cnts = imutils.grab_contours(cnts)
-    cnts = sort_contours(cnts, method="left-to-right")[0]
+
+    cnts = cv2.findContours(dilated, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0]
+    cnts = sort_contours(cnts)
     # loop over the contours
     for c in cnts:
-        if cv2.contourArea(c) > 10:
+        if cv2.contourArea(c) > 250:
             (x, y, w, h) = cv2.boundingRect(c)
             cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        roi = gray[y:y + h, x:x + w]
-        thresh = cv2.threshold(roi, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
-        thresh = cv2.resize(thresh, (32, 32), interpolation=cv2.INTER_CUBIC)
-        thresh = thresh.astype("float32") / 255.0
-        thresh = np.expand_dims(thresh, axis=-1)
-        thresh = thresh.reshape(1, 32, 32, 1)
-        ypred = model.predict(thresh)
-        ypred = LB.inverse_transform(ypred)
-        [x] = ypred
-        letters.append(x)
+            roi = gray[y:y + h, x:x + w]
+            thresh = cv2.threshold(roi, 0, 255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+            thresh = cv2.resize(thresh, (32, 32), interpolation = cv2.INTER_CUBIC)
+            thresh = thresh.astype("float32") / 255.0
+            thresh = thresh.reshape(1,32,32,1)
+            ypred = model.predict(thresh)
+            ypred = LB.inverse_transform(ypred)
+            letters.append(*ypred)
     return letters, image
 
 
